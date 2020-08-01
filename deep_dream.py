2cf79b21ddc04b44393cefa4aba3903ffb66fd4d
lowerthansound <email@lowerthansound.pro>
2020-07-31 22:10:30 -0300

    Structure code a bit more

--- deep_dream.py
+++ deep_dream.py
@@ -21,21 +21,65 @@ INPUT_DIR = pathlib.Path('input')
 OUTPUT_DIR = pathlib.Path('output')
 
 
-def dream(image, model, iterations, lr):
-    """ Updates the image to maximize outputs for n iterations """
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-    image = Variable(Tensor(image), requires_grad=True)
-    for i in range(iterations):
-        model.zero_grad()
-        out = model(image)
-        loss = out.norm()
-        loss.backward()
-        avg_grad = np.abs(image.grad.data.cpu().numpy()).mean()
-        norm_lr = lr / avg_grad
-        image.data += norm_lr * image.grad.data
-        image.data = clip(image.data)
-        image.grad.data.zero_()
-    return image.cpu().data.numpy()
+def main():
+    args = get_args()
+    model = get_model(args.layer)
+
+    print("Deep dream")
+    for image_path in INPUT_DIR.iterdir():
+
+        print(f'  {image_path}')
+
+        # Load image from input
+        image = Image.open(image_path)
+
+        # Deep dream
+        dreamed_image = deep_dream(
+            image,
+            model,
+            num_octaves=args.num_octaves,
+            octave_scale=args.octave_scale,
+            iterations=args.iterations,
+            lr=args.step_size,
+        )
+
+        # Save image to output
+        output = OUTPUT_DIR / image_path.name
+        plt.imsave(str(output), dreamed_image)
+
+
+def get_args():
+
+    print("Get arguments")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--layer", type=int, default=27, help="Layer to maximize output")
+    parser.add_argument("--num-octaves", type=int, default=10, help="Number of octaves")
+    parser.add_argument("--octave-scale", type=float, default=1.4, help="Image scale between octaves")
+    parser.add_argument("--iterations", type=int, default=20, help="Number of gradient ascent steps performed for each octave")
+    parser.add_argument("--step-size", type=float, default=0.01, help="Learning rate for the gradient ascent step")
+
+    return parser.parse_args()
+
+
+def get_model(layer):
+
+    # Layer indicates the layer we use for "optimizing" the image
+    # We will use the model up until that layer only
+
+    print("Get model")
+
+    # Retrieve layers from template model
+    template = models.vgg19(pretrained=True)
+    children = list(template.features.children())
+
+    # Create new (trimmed) model based on previous layers
+    new_model = nn.Sequential(*children[:layer+1])
+    if torch.cuda.is_available():
+        new_model = new_model.cuda()
+
+    return new_model
 
 
 def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
@@ -62,44 +106,21 @@ def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
     return deprocess(dreamed_image)
 
 
-def main():
-    print("Parse arguments")
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--at_layer", default=27, type=int, help="layer at which we modify image to maximize outputs")
-    parser.add_argument("--iterations", default=20, help="number of gradient ascent steps per octave")
-    parser.add_argument("--lr", default=0.01, help="learning rate")
-    parser.add_argument("--octave_scale", default=1.4, help="image scale between octaves")
-    parser.add_argument("--num_octaves", default=10, help="number of octaves")
-    args = parser.parse_args()
-
-    print("Load model")
-    network = models.vgg19(pretrained=True)
-    layers = list(network.features.children())
-    model = nn.Sequential(*layers[: (args.at_layer + 1)])
-    if torch.cuda.is_available():
-        model = model.cuda()
-
-    print("Deep dream images")
-    for image_path in INPUT_DIR.iterdir():
-
-        print(f'  {image_path}')
-
-        # Load image from input
-        image = Image.open(image_path)
-
-        # Deep dream
-        dreamed_image = deep_dream(
-            image,
-            model,
-            iterations=args.iterations,
-            lr=args.lr,
-            octave_scale=args.octave_scale,
-            num_octaves=args.num_octaves,
-        )
-
-        # Save image to output
-        output = OUTPUT_DIR / image_path.name
-        plt.imsave(str(output), dreamed_image)
+def dream(image, model, iterations, lr):
+    """ Updates the image to maximize outputs for n iterations """
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+    image = Variable(Tensor(image), requires_grad=True)
+    for i in range(iterations):
+        model.zero_grad()
+        out = model(image)
+        loss = out.norm()
+        loss.backward()
+        avg_grad = np.abs(image.grad.data.cpu().numpy()).mean()
+        norm_lr = lr / avg_grad
+        image.data += norm_lr * image.grad.data
+        image.data = clip(image.data)
+        image.grad.data.zero_()
+    return image.cpu().data.numpy()
 
 
 if __name__ == "__main__":
