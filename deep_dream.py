841ca0e25fa7cdfbd4c4c302930e7f44515068c3
lowerthansound <email@lowerthansound.pro>
2020-08-01 13:55:11 -0300

    Test layers

--- deep_dream.py
+++ deep_dream.py
@@ -20,122 +20,65 @@ from utils import deprocess, preprocess, clip
 
 INPUT_DIR = pathlib.Path('input')
 OUTPUT_DIR = pathlib.Path('output')
-CONFIGS = [
-
-    # num_octaves octave_scale iterations step_size
-
-    # [10, 1.4, 20, 0.01],
-
-    [ 1, 1.4, 4, 0.05],
-    [ 2, 1.4, 4, 0.05],
-    [ 3, 1.4, 4, 0.05],
-    [ 4, 1.4, 4, 0.05],
-    [ 5, 1.4, 4, 0.05],
-    [ 6, 1.4, 4, 0.05],
-    [ 7, 1.4, 4, 0.05],
-    [ 8, 1.4, 4, 0.05],
-    [ 8, 1.4, 4, 0.05],
-    [ 9, 1.4, 4, 0.05],
-    [10, 1.4, 4, 0.05],
-
-    [10, 1.1, 4, 0.05],
-    [10, 1.2, 4, 0.05],
-    [10, 1.3, 4, 0.05],
-    [10, 1.4, 4, 0.05],
-    # [10, 1.5, 4, 0.05], too small
-
-    [10, 1.4,  1, 0.01],
-    [10, 1.4,  2, 0.01],
-    [10, 1.4,  3, 0.01],
-    [10, 1.4,  4, 0.01],
-    [10, 1.4,  5, 0.01],
-    [10, 1.4, 10, 0.01],
-    [10, 1.4, 15, 0.01],
-    [10, 1.4, 18, 0.01],
-    [10, 1.4, 19, 0.01],
-    [10, 1.4, 20, 0.01],
-
-    [10, 1.4, 20, 0.01],
-    [10, 1.4, 20, 0.02],
-    [10, 1.4, 20, 0.10],
-    [10, 1.4, 20, 0.20],
-
-    [10, 1.4, 20, 0.01],
-    [10, 1.4, 10, 0.02],
-    [10, 1.4,  5, 0.04],
-
-]
-
 
 
 def main():
 
-    args = get_args()
-    model = get_model(args.layer)
-
+    print("Load image input")
     image = Image.open(INPUT_DIR / 'sky.jpeg')
 
-    for c in CONFIGS:
 
-        print(f"Processing image with params {c[0]:02d} {c[1]:.2f} {c[2]:02d} {c[3]:.2f}")
-
-        t0 = time()
-
-        # Dream image
-        dreamed_image = deep_dream(
-            image,
-            model,
-            num_octaves=c[0],
-            octave_scale=c[1],
-            iterations=c[2],
-            lr=c[3],
-        )
-
-        t1 = time()
-
-        # Save image to output
-        name = f"output_{c[0]:02d}-{c[1]:.2f}-{c[2]:02d}-{c[3]:.2f}.jpeg"
-        output = OUTPUT_DIR / name
-        plt.imsave(str(output), dreamed_image)
-
-        # Save timings
-        elapsed_time = t1 - t0
-        with (OUTPUT_DIR / 'timings.txt').open(mode='a') as f:
-            f.write(f"{name}: {elapsed_time:.1f}s\n")
+    print("Load base model")
+    base_model = models.vgg19(pretrained=True)
+    base_layers = list(base_model.features.children())
 
 
-def get_args():
+    nlayers = len(base_layers)
+    print(f"This model has {nlayers} layers")
+    for layer in range(nlayers):
 
-    print("Get arguments")
+        try:
 
-    parser = argparse.ArgumentParser()
+            print("Creating model up until layer {layer}")
 
-    parser.add_argument("--layer", type=int, default=27, help="Layer to maximize output")
-    parser.add_argument("--num-octaves", type=int, default=10, help="Number of octaves")
-    parser.add_argument("--octave-scale", type=float, default=1.4, help="Image scale between octaves")
-    parser.add_argument("--iterations", type=int, default=20, help="Number of gradient ascent steps performed for each octave")
-    parser.add_argument("--step-size", type=float, default=0.01, help="Learning rate for the gradient ascent step")
+            t0 = time()
 
-    return parser.parse_args()
+            new_model = nn.Sequential(*base_layers[0:layer+1])
+            if torch.cuda.is_available():
+                new_model = new_model.cuda()
 
+            print("Dream")
 
-def get_model(layer):
+            t1 = time()
 
-    # Layer indicates the layer we use for "optimizing" the image
-    # We will use the model up until that layer only
+            # Deep dream image
+            dreamed_image = deep_dream(
+                image,
+                model,
+                num_octaves=10,
+                octave_scale=1.4,
+                iterations=10,
+                lr=0.1,
+            )
 
-    print("Get model")
+            t2 = time()
 
-    # Retrieve layers from template model
-    template = models.vgg19(pretrained=True)
-    children = list(template.features.children())
+            print("Saving output and timings")
 
-    # Create new (trimmed) model based on previous layers
-    new_model = nn.Sequential(*children[:layer+1])
-    if torch.cuda.is_available():
-        new_model = new_model.cuda()
+            # Save image to output
+            name = f"output_layer-{layer:04d}.jpeg"
+            plt.imsave(str(OUTPUT_DIR / name), dreamed_image)
 
-    return new_model
+            # Save timings
+            dreaming_time = t2 - t1
+            with (OUTPUT_DIR / 'timings.txt').open(mode='a') as f:
+                f.write(f"{name}: {dreaming_time:.1f}s\n")
+
+        except Exception as e:
+
+            print(f"Error at layer {layer}: {e}")
+            with (OUTPUT_DIR / 'errors.txt').open(mode='a') as f:
+                f.write(f"Error at layer {layer:04d}: {e}\n")
 
 
 def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
